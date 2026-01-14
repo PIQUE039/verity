ae3b0d35d195cc3b74be0576eee980f45f9288aa
Purvesh Kirlosker <purvesh367@gmail.com>
2026-01-15 00:50:10 +0530

    Backend auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes')
+const mongoose = require('mongoose');
 
 //CONFIGURATION
 dotenv.config();
@@ -10,15 +12,28 @@ const app = express();
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
+app.use('/api/auth', authRoutes);
 
 //ROUTES
 app.get('/',(req,res) => {
    res.send('API is running...');
 });
 
-// STARTING THE SERVER
-const PORT = process.env.PORT || 5000;
+//Connecting Database Logic
+const connectDB = async() => {
+   try{
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log('Local MongoDB connected successfully');
 
-app.listen(PORT, () => [
-   console.log(`Server is running on prot ${PORT}`)
-]);
+      //starting the server if the connection is successfull
+      const PORT = process.env.PORT || 5000;
+      app.listen(PORT, () => [
+         console.log(`Server is running on prot ${PORT}`)
+      ]);
+   }catch(error){
+      console.error('MongoDB connection failed:',error.message);
+      process.exit(1);
+   }
+};
+
+connectDB();
